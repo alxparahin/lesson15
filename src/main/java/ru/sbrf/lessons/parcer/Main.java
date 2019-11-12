625b4766ea3e55afc9b278c0d0b4bfd80ad38402
alxparahin <Parakhin.A.Vla@sberbank.ru>
2019-11-12 19:20:42 +0300

    Initial commit

--- src/main/java/ru/sbrf/lessons/parcer/Main.java
+++ src/main/java/ru/sbrf/lessons/parcer/Main.java
@@ -10,10 +10,16 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Loader loader = new HTTPLoader();
+        download(loader);
+
+        loader = new SSHLoader();
+        download(loader);
+    }
+
+    public static void download(Loader loader){
         ConnectionProperties connectionProperties = new ConnectionProperties("bob", "123456", "www.ru");
         FileFilter fileFilter = new FileFilter("*", new Date());
 
-        loader = new SSHLoader();
         loader.download(connectionProperties, fileFilter);
     }
 }
